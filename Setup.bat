94418de96c7ee31689ffd6ee23d6c04e06c66a62
FireCat999 <100002098+FireCat999@users.noreply.github.com>
2023-03-22 16:28:24 -0700

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,5 @@
+RD /S /Q C:\OSPanel\domains\pizza\
+xcopy ".\site\" "C:\OSPanel\domains\pizza\" /S /E
+C:\OSPanel\modules\database\MySQL-8.0-Win10\bin\mysql.exe --user=root --port=3306 -e "drop database pizza_db;"
+C:\OSPanel\modules\database\MySQL-8.0-Win10\bin\mysql.exe --user=root --port=3306 -e "create database pizza_db;"
+C:\OSPanel\modules\database\MySQL-8.0-Win10\bin\mysql.exe --user=root --port=3306 --database=pizza_db -e "source .\db\pizza_db.sql"
